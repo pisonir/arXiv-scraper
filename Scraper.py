c7ec2ed31b44ff73f8c6458d52db5d3484c3bec3
pisonir <riccardopisoni91@gmail.com>
2019-06-23 22:37:40 +0200

    cleaning up code

--- Scraper.py
+++ Scraper.py
@@ -2,7 +2,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 from tkinter import *
 import webbrowser
-import math
+
 
 
 class Scraper:
@@ -10,21 +10,39 @@ class Scraper:
         self.site = site
 
     def weblink(self, *args):
+        '''
+        Open the .pdf document corresponding to the link that has been selected by the user.
+        :param args: event of selecting an item in the listbox
+        :return: None
+        '''
         index = self.titles_list.curselection()[0]
         item = self.titles_list.get(index)
         if 'https://' in item:
             webbrowser.open_new(item)
-        
-    def scrape(self):
-        r = urllib.request.urlopen(self.site)
-        html = r.read() # all of the HTML from the website is in the variable html
+
+    def open_and_parse(self):
+        '''
+        Open and parse the website we are interested in, in this example is arXiv.
+        :return: None
+        '''
+        r = urllib.request.urlopen(self.site)  # to access the website (in this example is arXiv)
+        html = r.read()  # all of the HTML from the website is in the variable html
         parser = "html.parser"
-        sp = BeautifulSoup(html, parser) # The BeautifulSoup object does all the hard work and parses the HTML
+        self.sp = BeautifulSoup(html, parser)  # The BeautifulSoup object does all the hard work and parses the HTML
+
+    def scrape(self, keyword):
+        '''
+        Scrape the latest arXiv publications and select all the titles that include keyword.
+        :param keyword: str
+        :return:
+        '''
+        self.open_and_parse()
+        # CREATE a LIST of items that contain the KEYWORD and present them on the root window using the Listbox class
         self.titles_list = Listbox(root, yscrollcommand = scrollbar.set, bg = 'white', font=('times',13))
         self.titles_list.bind('<<ListboxSelect>>', self.weblink)
-        for tag in sp.find_all("span", class_="descriptor", text="Title:"):
+        for tag in self.sp.find_all("span", class_="descriptor", text="Title:"):
             title = tag.next_sibling.string
-            if 'graphene' in title.lower():
+            if keyword in title.lower():
                 self.titles_list.insert(END, title)
                 # the next line of code is really ugly but I haven't found a better solution yet
                 link_1 = tag.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element
@@ -44,19 +62,21 @@ class Scraper:
                     self.titles_list.insert(END, url)
                     self.titles_list.insert(END, '')
         self.titles_list.pack(side = LEFT, fill = BOTH, expand = YES)
+        # ADD the SCROLLBAR
         scrollbar.pack(side=RIGHT, fill=Y)
         scrollbar.config(command=self.titles_list.yview)
         for i in range(self.titles_list.size()):
             # to highlight the url items
             if 'https://' in self.titles_list.get(i):
-                self.titles_list.itemconfig(i, bg='yellow')
+                self.titles_list.itemconfig(i, bg='lightyellow')
 
 
 if __name__ == "__main__":
     news = "https://arxiv.org/list/cond-mat.mes-hall/new/"
     root = Tk()  # root is an instance of the class Tk. Main window of the application
-    scrollbar = Scrollbar(root)
-    Scraper(news).scrape()
+    keyword = input('Please enter a keyword \n')
+    scrollbar = Scrollbar(root) # to add a scrollbar on the root window
+    Scraper(news).scrape(keyword) # To scrape all the titles on news that include the word 'graphene'
     root.title('Today arXiv publications')
     root.mainloop()  # mainloop() is an ininite loop used to run the application...until an event occurs such as closing
     # the window
